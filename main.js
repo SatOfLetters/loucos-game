7c6fbe23055564fcadc7fcf4d337a08b1c373022
SatOfLetters <97686395+SatOfLetters@users.noreply.github.com>
2022-04-29 14:48:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -86,3 +86,24 @@ document.body.onscroll = function() {
 
 
 
+
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+    content.style.maxHeight = null;
+    } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+    }
+});
+}
+
+document.querySelector('.opened').click()
